c011c302fe0ff158dd5c0e105ae9544924e8c026
Chénel <julien.chenel@gmail.com>
2018-02-28 13:06:45 +0100

    valeur Pamiers par default OK

--- js/app.js
+++ js/app.js
@@ -2,6 +2,27 @@ moment.locale('fr');
 $("#date").text(moment().format('LL'));
 
 
+        var ville = $("#champsVille").val();
+        console.log(ville);
+
+        $.ajax({
+
+            url: 'http://api.openweathermap.org/data/2.5/weather?q=' + ville + "&units=metric" + "&APPID=693847a822e4a79db242ae4969159f0e",
+            type: "GET",
+            dataType: "jsonp",
+            success: function(data) {
+                var tempCourante = tempData(data);
+                var autresValeurs = otherDatas(data);
+
+                $("#cadreTemp").html(tempCourante);
+
+                $("#autresDonnees").html(autresValeurs);
+
+            }
+
+        });
+        
+
 $(document).ready(function() {
 
     $("#valider").click(function() {
@@ -16,11 +37,11 @@ $(document).ready(function() {
             dataType: "jsonp",
             success: function(data) {
                 var tempCourante = tempData(data);
-                // var autresValeurs = otherDatas(data);
+                var autresValeurs = otherDatas(data);
 
                 $("#cadreTemp").html(tempCourante);
 
-                // $("#autresDonnees").html(autresValeurs);
+                $("#autresDonnees").html(autresValeurs);
 
             }
 
@@ -31,20 +52,13 @@ $(document).ready(function() {
 });
 
 function tempData(data) {
-    return + data.main.temp + "°";
-
-
-        // "<h3>weather : " + data.weather.main.temp_min + "</h3>" +
-        // "<h3>weather : " + data.main.temp_max + "</h3>" +
-        // "<h3>weather : " + data.main.pressure.value + "</h3>" +
-        // "<h3>weather : " + data.main.wind.speed.value + "</h3>" +
-        // "<h3>weather : " + data.main.humidity.value + "</h3>" + ;
+    return + Math.ceil(data.main.temp) + "°";
 }
 
-// function otherDatas(data) {
-//     return "<h3>weather : " + data.weather.main.temp_min + "</h3>" +
-//         "<h3>weather : " + data.main.temp_max + "</h3>";
-        // "<h3>weather : " + data.main.pressure.value + "</h3>" +
-        // "<h3>weather : " + data.main.wind.speed.value + "</h3>" +
-        // "<h3>weather : " + data.main.humidity.value + "</h3>";
-// }
+function otherDatas(data) {
+    return "Temp. max : " + data.main.temp_max + "°<br/>" +
+        "Temp. min : " + data.main.temp_min + "°<br/>" +
+        "Pres. atmosphérique : " + data.main.pressure + "<br/>" +
+        "Vitesse du vent : " + Math.ceil(data.wind.speed) + "km/h<br/>" +
+        "Humidité : " + data.main.humidity + "%";
+}
